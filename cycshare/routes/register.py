b70945a3d1cfcbb6e6f88f0d2d2032fc63d51a2e
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-09-26 22:31:49 +0200

    working on registration form

--- cycshare/routes/register.py
+++ cycshare/routes/register.py
@@ -30,17 +30,17 @@ lastname_input = dbc.FormGroup(
 
 email_input = dbc.FormGroup(
     [
-        dbc.Label("Email", html_for="example-email"),
-        dbc.Input(type="email", id="example-email", placeholder="Enter email"),
+        dbc.Label("Email", html_for="input-email"),
+        dbc.Input(type="email", id="input-email", placeholder="Enter email"),
     ]
 )
 
 password_input = dbc.FormGroup(
     [
-        dbc.Label("Password", html_for="example-password"),
+        dbc.Label("Password", html_for="input-password"),
         dbc.Input(
             type="password",
-            id="example-password",
+            id="input-password",
             placeholder="Enter password",
         ),
     ]
@@ -48,10 +48,10 @@ password_input = dbc.FormGroup(
 
 password_input_2 = dbc.FormGroup(
     [
-        dbc.Label("Repeat Password", html_for="example-password-2"),
+        dbc.Label("Repeat Password", html_for="input-password-2"),
         dbc.Input(
             type="password",
-            id="example-password-2",
+            id="input-password-2",
             placeholder="Enter password again",
         ),
     ]
@@ -69,10 +69,11 @@ read=dcc.Checklist(
 #[dbc.Form([email_input, password_input])]
 dashapp.layout=dbc.Row( [
     dbc.Col( md=4),
-    dbc.Col( dbc.Card(  dbc.Form([ html.H2("Register", style={'textAlign': 'center'} ),
+    dbc.Col( [ html.Div(id="app_access"),
+               dbc.Card(  dbc.Form([ html.H2("Register", style={'textAlign': 'center'} ),
                                     dbc.Row([ 
-                                        dbc.Col(firstname_input),  
-                                        dbc.Col(lastname_input)] ),
+                                        dbc.Col([ firstname_input,html.Div(id="firstname-feedback")] ),  
+                                        dbc.Col([ lastname_input,html.Div(id="lastname-feedback")] )] ),
                                     email_input,
                                     html.Div(id="email-feedback"),
                                     password_input,
@@ -82,11 +83,30 @@ dashapp.layout=dbc.Row( [
                                     dbc.Row([ read , 
                                               html.A("User Agreement and Data Privacy Statment.", href="https://www.sapo.pt",style={"margin-left":"4px",'whiteSpace': 'pre-wrap'})]),
                                     html.Div(id="checkbox-feedback"),
-                                    html.Button(id='submit-button-state', n_clicks=0, children='Submit', style={"width":"auto","margin-top":4, "margin-bottom":4})
+                                    html.Button(id='submit-button-state', n_clicks=0, children='Submit', style={"width":"auto","margin-top":4, "margin-bottom":4}),
+                                    html.Div(id="submission-feedback"),
                                 ])
-                        , body=True),
+                        , body=True)],
              md=4, align="center"),
     dbc.Col( md=4),
 ],
 align="center",
 style={"min-height": "100vh", 'verticalAlign': 'center'})
+
+@dashapp.callback(
+    Output('firstname-feedback', 'children'),
+    Output('lastname-feedback', 'children'),
+    Output('email-feedback', 'children'),
+    Output('pass-feedback', 'children'),
+    Input('submit-button-state', 'n_clicks'),
+    State('first_name', 'value'),
+    State('last_name', 'value'),
+    State('input-email', 'value'),
+    State('input-password', 'value'),
+    prevent_initial_call=True
+    )
+def test_out(n_clicks,first_name, last_name, email,passA, passB):
+    return dbc.Alert( first_name , color="warning"), dbc.Alert( last_name , color="warning"), dbc.Alert( email , color="warning"),dbc.Alert( passA , color="warning")
+
+
+
