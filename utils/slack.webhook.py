6720592143bdb7a10c2d24a147f08e30307e9f9f
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-10-08 16:07:41 +0200

    nightly builds on local machines

--- utils/slack.webhook.py
+++ utils/slack.webhook.py
@@ -6,19 +6,22 @@ parser = argparse.ArgumentParser(description="Send data to a Slack webhook.")
 parser.add_argument('-w', metavar="webhook", type=str, nargs='?', help="Webhook URL", required=True)
 parser.add_argument('-s', metavar="subject", type=str, nargs='*', help="Message subject (required)", required=True)
 parser.add_argument('-f', metavar="file", type=str, nargs='?', help="txt file to be sent as text.")
+parser.add_argument("--short", metavar="type", type=str, nargs='?', choices=["nightly"] , default="nightly")
 args = parser.parse_args()
 
 import json
 import requests
 
+msg=" ".join(args.s)
+
 slack_data_long={
-    "text": " ".join(args.s) ,
+    "text":  msg,
     "blocks": [
 		{
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "*"+" ".join(args.s)+"*"
+				"text": "*"+msg+"*"
 			}
 		},
 		{
@@ -30,14 +33,65 @@ slack_data_long={
 if args.f :
     f = open( args.f , 'r')
     f = f.readlines()
-    f = "".join(f)
+
     slack_data_long_file={
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": f
-			}
-		}
+        "type": "section",
+        "text": {
+            "type": "plain_text",
+            "text": "".join(f)
+        }
+    }
+
+    if args.short == "nightly" :
+        summary=[ f[4], f[6] ]
+        f = "".join(f)
+        list_of_checks=[
+            ":: build linux/amd64 myapp:nightly",
+            ":: push linux/amd64 myapp:nightly" , 
+            ":: build and pushed linux/arm64 myapp:nightly" ,
+            ":: build linux/amd64 myapp-dockerize:nightly", 
+            ":: push linux/amd64 myapp-dockerize:nightly",
+            ":: build and push linux/arm64 myapp-dockerize:nightly"
+        ]
+
+        success=True
+        for c in list_of_checks :
+            if  c in f :
+                summary.append(c+"\n")
+            else:
+                summary.append(c.replace(":: ", ":: !FAILED! ")+"\n")
+                success=False
+
+        summary="".join(summary)
+
+        if success :
+            msg=msg+" (success)"
+        else:
+            msg=msg+" (FAILED)"
+
+        slack_data_long={
+            "text":  msg,
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*"+msg+"*"
+                    }
+                },
+                {
+                    "type": "divider"
+                }
+            ]
+        }
+        
+        slack_data_long_file={
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": summary
+                }
+            }
     slack_data_long["blocks"]=slack_data_long["blocks"]+[slack_data_long_file]
 
 response = requests.post(
