e99e54d14fc5981258f181578440f62d8fe547da
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-12-28 23:33:45 +0100

    added tls to email

--- config.py
+++ config.py
@@ -39,8 +39,10 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # print("MAIL PASSWORD", MAIL_PASSWORD)
     INSTANCE = os.environ.get('INSTANCE') or "PRODUCTION"
     COMMIT = commit
     ADMINS = os.environ.get('ADMINS').split(",") or ['jboucas@gmail.com']
