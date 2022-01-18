a1f0c3a209e578c83c7735dcb30bf4eabb7dca58
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-01-18 13:10:46 +0100

    config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ class Config(object):
     INSTANCE = os.environ.get('INSTANCE') or "PRODUCTION"
     COMMIT = commit
     ADMINS = os.environ.get('ADMINS').split(",") or ['jboucas@gmail.com']
-    PREAUTH = os.environ.get('PREAUTH').split(",") or True
+    PREAUTH = os.environ.get('PREAUTH') or True
 
     # PRIVATE_APPS = os.environ.get('PRIVATE_APPS') or None
 
