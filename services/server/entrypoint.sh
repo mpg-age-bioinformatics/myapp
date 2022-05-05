03540ec75bd77d4798665a9646bd96a559e374a7
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-05-05 14:12:59 +0200

    cron monitoring

--- services/server/entrypoint.sh
+++ services/server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$FLASK_ENV" == "rsync" ]] ; 
   then 
-    echo "5 * * * * /${BUILD_NAME}/utils/owncloud.client.py" > /${BUILD_NAME}_data/owncloud.cron
+    echo "5 * * * * /${BUILD_NAME}/utils/owncloud.client.py > /owncloud.log 2>&1" > /${BUILD_NAME}_data/owncloud.cron
     crontab /${BUILD_NAME}_data/owncloud.cron
     rm /${BUILD_NAME}_data/owncloud.cron
 fi
