fc187d711138161d3a8cdec3e203cb47eefc7a87
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-05-05 15:18:10 +0200

    service/rsync

---
+++ services/rsync/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "*/5 * * * * /${BUILD_NAME}/utils/owncloud.client.py > /owncloud.log 2>&1" > /${BUILD_NAME}_data/owncloud.cron
+crontab /${BUILD_NAME}_data/owncloud.cron
+rm /${BUILD_NAME}_data/owncloud.cron
+
+tail -f /dev/null
+
+
--- services/server/entrypoint.sh
+++ services/server/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [[ "$FLASK_ENV" == "rsync" ]] ; 
-  then 
-    echo "5 * * * * /${BUILD_NAME}/utils/owncloud.client.py > /owncloud.log 2>&1" > /${BUILD_NAME}_data/owncloud.cron
-    crontab /${BUILD_NAME}_data/owncloud.cron
-    rm /${BUILD_NAME}_data/owncloud.cron
-fi
-
 
 while ! mysql --user=${MYSQL_USER} --password="${MYSQL_PASSWORD}" --host=${MYSQL_HOST} -e "use ${DB_NAME}"; 
   do 
