ca83dc6a26a20bf2a9196533e02a298f8d7dcad6
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-05-16 18:42:55 +0200

    oc client: adding verbosity

--- services/rsync/entrypoint.sh
+++ services/rsync/entrypoint.sh
@@ -4,7 +4,7 @@ if [ -e /${BUILD_NAME}/.owncloud.flaski  ] ;
 
     then
 
-        echo "*/15 * * * * /${BUILD_NAME}/utils/owncloud.client.py --config /${BUILD_NAME}/.owncloud.flaski > /owncloud.log 2>&1" > /${BUILD_NAME}_data/owncloud.cron
+        echo "*/15 * * * * /${BUILD_NAME}/utils/owncloud.client.py -v --config /${BUILD_NAME}/.owncloud.flaski > /owncloud.log 2>&1" > /${BUILD_NAME}_data/owncloud.cron
         crontab /${BUILD_NAME}_data/owncloud.cron
         rm /${BUILD_NAME}_data/owncloud.cron
 
