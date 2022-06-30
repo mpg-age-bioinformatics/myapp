d40fcb4f71ba44e1dbc71d474dfcc33b7d8f09f2
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-06-30 12:10:19 +0200

    better backup entrypoint

--- services/backup/entrypoint.sh
+++ services/backup/entrypoint.sh
@@ -16,20 +16,21 @@ do echo "Waiting for mysql.. " && sleep 4
 done
 echo "Found ${DB_NAME} db."
 
-if [ "${INIT_BACKUP}" -gt "0" ]; 
-then
-    echo "=> Create a backup on the startup"
-    /${BUILD_NAME}/services/backup/backup.sh
-elif [ -n "${INIT_RESTORE_LATEST}" ]; 
-then
-    echo "=> Restore latest backup"
-    until nc -z "$MYSQL_HOST" "$MYSQL_PORT"
-        do
-            echo "waiting database container..."
-            sleep 1
-    done
-    find /backup/mariadb -maxdepth 1 -name 'latest.myapp.sql.gz' | tail -1 | xargs /${BUILD_NAME}/services/backup/restore.sh
-fi
+# RESTORES NO LONGER TAKE PLACE FROM THE BACKUP CONTAINER
+# if [ "${INIT_BACKUP}" == "1" ] ; 
+# then
+#     echo "=> Create a backup on the startup"
+#     /${BUILD_NAME}/services/backup/backup.sh
+# elif [ -n "${INIT_RESTORE_LATEST}" ] ; 
+# then
+#     echo "=> Restore latest backup"
+#     until nc -z "$MYSQL_HOST" "$MYSQL_PORT"
+#         do
+#             echo "waiting database container..."
+#             sleep 1
+#     done
+#     find /backup/mariadb -maxdepth 1 -name 'latest.${DB_NAME}.sql.gz' | tail -1 | xargs /${BUILD_NAME}/services/backup/restore.sh
+# fi
 
 echo "${CRON_TIME} /${BUILD_NAME}/services/backup/backup.sh >> /backup/mysql_backup.log 2>&1" > /crontab.conf
 # echo "${CRON_TIME} rsync -rtvh --delete /myapp_data/users/ /backup/users_data/ >> /backup/rsync.log 2>&1" >> /crontab.conf
