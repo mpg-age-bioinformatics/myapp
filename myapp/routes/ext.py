0c13480e20499ee7308acbbbb18609b6be9c3f56
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-11-29 13:55:09 +0100

    break-word for large links

--- myapp/routes/ext.py
+++ myapp/routes/ext.py
@@ -39,38 +39,6 @@ def make_layout(pathname):
     target=pathname.split(f"{PAGE_PREFIX}/ext/")[-1]
     target=f'https://{target}'
 
-    # target=None
-
-    # if current_user :
-    #     if current_user.is_authenticated :
-    #         if current_user.active :
-    #             target=f"{PAGE_PREFIX}/home/"
-    #             open_content=html.Div(id="page-footer",style={"height":"10px"})
-    #             refresh=True
-
-    # if not target:
-    #     if pathname not in [f'{PAGE_PREFIX}/index/open/', f'{PAGE_PREFIX}/open/'] :
-    #         target=f"{PAGE_PREFIX}/index/open/"
-    #         open_content=html.Div(id="page-footer", style={"height":"10px"})
-    #         refresh=False
-
-    #     else :
-    #         target=f"{PAGE_PREFIX}/index/"
-    #         open_content=html.Div([
-    #                         dbc.Row( 
-    #                             html.Footer( [ 
-    #                                 html.A("About", style=links_style, href=f"{PAGE_PREFIX}/about/"),
-    #                                 html.A("Login", style=links_style, href=f"{PAGE_PREFIX}/login/"),
-    #                                 html.A("Register", style=links_style, href=f"{PAGE_PREFIX}/register/"),
-    #                                 html.A("Contact", style=links_style, href=f"{PAGE_PREFIX}/contact/")
-    #                             ] , 
-    #                             style={"margin-top": 25, "margin-bottom": 5,},
-    #                             ),
-    #                             style={"justify-content":"center"}
-    #                         )
-    #                     ],style={"height":"10px"})
-    #         refresh=False
-
     app_title=app.config["APP_TITLE"]
 
     page_content=html.Div(
@@ -84,27 +52,28 @@ def make_layout(pathname):
                             dcc.Link(
                                 [
                                     # html.Img( src='data:image/png;base64,{}'.format(encoded_image.decode() ) , height="300px", style={ "margin-bottom":5}),
-                                    html.H3([ f"You are leaving {app_title} and being redirected to",\
+                                    html.H3([ f"You are leaving {app_title} and being redirected to:",\
                                         html.Br(),\
                                         html.Br(),\
                                         target,\
                                         html.Br(),\
                                         html.Br(),\
                                         "Click here to continue."], 
-                                        style={"textAlign":"center"}),
+                                        style={"textAlign":"center","overflow-wrap": "break-word"}),
                                 ],
                                 href=target,
                                 refresh=True,
-                                style={"color":"black","text-decoration": "none"}                        ),
+                                style={"color":"black","text-decoration": "none"}                        
+                                ),
                         ], 
                         sm=9,md=7, lg=5, xl=5, 
                         align="center",
-                        style={"textAlign":"center"},
+                        style={"textAlign":"center", "height": "100%" },
                     ),
                     navbar_A,
                 ],
                 justify="center",
-                style={"min-height": "100vh"}
+                style={"min-height": "100vh", "margin-bottom":"0px","margin-left":"5px","margin-right":"5px"}
             )
         ]
     )
